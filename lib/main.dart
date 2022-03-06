e03a0e9310bb4d8bc1c57afa095d62a80f0e7096
Mahmudul-Sumoon <sumoon14@cse.pstu.ac.bd>
2022-03-07 03:53:29 +0600

    url loader with memorized power

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
+
+// void TestIt() {
+//   final values = [1, 2, null, 4];
+//   final v = values.compactMap();
+// }
 
 void main() {
   runApp(const MyApp());
@@ -20,33 +35,35 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const url =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png';
+
 class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller],
+    final s = useMemoized(
+      () => NetworkAssetBundle(Uri.parse(url))
+          .load(url)
+          .then((data) => data.buffer.asUint8List())
+          .then((data) => Image.memory(data)),
     );
+    final image = useFuture(s);
+    // print(image.hasData);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: controller,
-          ),
-          Text('you type ${text.value}'),
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            image.hasData ? image.data! : null,
+          ].compactMap().toList(),
+        ),
       ),
     );
   }
