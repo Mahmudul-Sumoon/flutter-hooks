e413e3edc0bf0498a47dcd60eaf60a68ad8bfe3a
Mahmudul-Sumoon <sumoon14@cse.pstu.ac.bd>
2022-03-07 16:28:36 +0600

    countDown

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> compactMap<E>([
-    E? Function(T?)? transform,
-  ]) =>
-      map(
-        transform ?? (e) => e,
-      ).where((e) => e != null).cast();
-}
-
-// void TestIt() {
-//   final values = [1, 2, null, 4];
-//   final v = values.compactMap();
-// }
 
 void main() {
   runApp(const MyApp());
@@ -35,36 +23,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const url =
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png';
+//const url =
+// 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png';
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
+        .takeWhile((value) => value >= 0)
+        .listen((value) {
+      this.value = value;
+    });
+  }
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
 
 class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final s = useMemoized(
-      () => NetworkAssetBundle(Uri.parse(url))
-          .load(url)
-          .then((data) => data.buffer.asUint8List())
-          .then((data) => Image.memory(data)),
-    );
-    final image = useFuture(s);
-    // print(image.hasData);
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-            image.hasData ? image.data! : null,
-          ].compactMap().toList(),
-        ),
-      ),
+      body: Center(child: Text(notifier.value.toString())),
     );
   }
 }
